136cf58e630dae6e29094cf7758a7b45d145f578
Nguyễn Tấn Nhi <144986314+Tan-Nhi@users.noreply.github.com>
2026-03-23 23:05:24 +0700

    setup base project

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,16 @@ const App = () => {
 
   return (
     <>
-
+      <div className="app">
+        <div className="bg-grid" />
+        <div className="container">
+          <div>todo list</div>
+        </div>
+        <div>Header</div>
+        <div>statsbar</div>
+        <div>fillterBar</div>
+        <div>TaskBoard</div>
+      </div>
     </>
   )
 }
